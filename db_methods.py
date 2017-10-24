cbbd65e89892e378ae835e3d7d457a7116325f09
Jake Goldman <jgoldman@stuy.edu>
2017-10-24 16:14:06 -0400

    Wrote a basic add_user method

--- db_methods.py
+++ db_methods.py
@@ -7,4 +7,13 @@ c = db.cursor()
 
 # add a user to the users table
 def add_user( user_id, username, password ):
-    command = "INSERT INTO users VALUES( %s, %s, %s )"
+    command = "INSERT INTO users VALUES( %d, %s, %s )" % (user_id, repr(username), repr(password))
+    c.execute(command)
+    db.commit()
+
+
+# testing
+# add_user(0, "testname", "testpass")
+
+db.commit()
+db.close()
