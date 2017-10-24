f88e652f9aba615888be03f39d1d088bc88588f1
Jake Goldman <jgoldman@stuy.edu>
2017-10-24 14:50:49 -0400

    started database commands

--- db_create.py
+++ db_create.py
@@ -2,17 +2,21 @@ import sqlite3
 
 
 db = sqlite3.connect("app.db")
-c = db.cursor
+c = db.cursor()
 
 # create a table of users
 command = "CREATE TABLE users (user_id INTEGER PRIMARY KEY, username TEXT,  password TEXT );"
 db.execute( command )
 
+# create a table of stories
 command = "CREATE TABLE stories (story_id INTEGER PRIMARY KEY, title TEXT, body TEXT, completed INTEGER );"
 db.execute( command )
 
+# create a table of contributions
 command = "CREATE TABLE contributions (user_id INTEGER, story_id INTEGER,  edit_stamp TEXT, edit TEXT );"
 db.execute( command )
 
+
+# save changes and close
 db.commit()
 db.close()
--- db_methods.py
+++ db_methods.py
@@ -1,1 +1,10 @@
 import sqlite3
+
+# open the database
+db = sqlite3.connect("app.db")
+c = db.cursor()
+
+
+# add a user to the users table
+def add_user( user_id, username, password ):
+    command = "INSERT INTO users VALUES( %s, %s, %s )"
