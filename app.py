394b784f65cdea7921ecdbf1f20d788ff5915621
Jake Goldman <jgoldman@stuy.edu>
2017-10-24 14:38:28 -0400

    wrote database creation, created app.db

---
+++ app.py
@@ -0,0 +1,15 @@
+import sqlite3
+import csv
+
+from flask import Flask, flash, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hi"
+
+@app.route('/profile')
+def profile():
+    return "profile"
+
--- db_create.py
+++ db_create.py
@@ -4,6 +4,15 @@ import sqlite3
 db = sqlite3.connect("app.db")
 c = db.cursor
 
-command = "CREATE TABLE "
+# create a table of users
+command = "CREATE TABLE users (user_id INTEGER PRIMARY KEY, username TEXT,  password TEXT );"
+db.execute( command )
 
+command = "CREATE TABLE stories (story_id INTEGER PRIMARY KEY, title TEXT, body TEXT, completed INTEGER );"
+db.execute( command )
+
+command = "CREATE TABLE contributions (user_id INTEGER, story_id INTEGER,  edit_stamp TEXT, edit TEXT );"
+db.execute( command )
+
+db.commit()
 db.close()
