0443cb7404e5734d566fdaa13f27d802a483406f
Jake Goldman <jgoldman@stuy.edu>
2017-10-24 14:53:54 -0400

    merge conflicts again?

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import sqlite3
-import csv
-
-=======
->>>>>>> 846d2b0a08aad64988bc69ec6c3e5c2da61e71aa
 from flask import Flask, flash, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
@@ -16,3 +10,14 @@ def home():
 def profile():
     return "profile"
 
+@app.route('/stories')
+def stories():
+    return "stories"
+
+@app.route('/createStory')
+def createStory():
+    return "create story"
+
+@app.route('/contributeStory')
+def contributeStory():
+    return "contribute to story"
