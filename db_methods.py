b6592c6c5eec280fc26c08f6ab988b33b72e2f91
Jake Goldman <jgoldman@stuy.edu>
2017-10-25 14:43:07 -0400

    started writing add_cont, need to finish/clean it up

--- db_methods.py
+++ db_methods.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 # open the database
 db = sqlite3.connect("app.db")
@@ -11,25 +12,36 @@ def add_user( user_id, username, password ):
     c.execute(command)
     db.commit()
 
+    
 # add a story to the story table
 def add_story( story_id, title, body, completed ):
-    command = "INSERT INTO stories VALUES( %d, %s, %s, %d )" % (user_id, repr(title), repr(body), completed)
+    command = "INSERT INTO stories VALUES( %d, %s, %s, %d )" % (story_id,repr(title), repr(body), completed)
     c.execute(command)
     db.commit()
 
+    
 # add an edit to the contribution table AND update the story
 # NOTE: doesn't currently mark as completed, will do in the future
 def add_cont( user_id, story_id, addition ):
-    #get the timestamp for the contribution
-    
+    # get the timestamp for the contribution
+    timestamp = str( datetime.now() )
+    # making the actual contribution
     command = "INSERT INTO contributions VALUES( %d, %d, %s, %s )" % (user_id, story_id, repr(timestamp), repr(addition) )
     c.execute(command)
+    
+    # update the body of the story
+    command = "SELECT body FROM stories WHERE stories.story_id = contributions.%d" % (story_id)
+    body = c.execute(command)[0]
+    body = body + "\n\n" + addition
+    command = "UPDATE stories SET body = %s WHERE stories.story_id = contridbutions.%d" % ( body, story_id )
     db.commit()
 
 
     
-# tests
-# add_user(0, "testname", "testpass")
+# extremely basic tests
+add_user(0, "john doe", "random")
+add_story(0, "the room", "this is a bad movie.", 0)
+add_cont        ( 0, 0, "I really don't reccomend it")
 
 db.commit()
 db.close()
