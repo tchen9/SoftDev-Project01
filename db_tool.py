e29f213f04201fad903c0749d36810bd805680ee
Jake Goldman <jgoldman@stuy.edu>
2017-10-26 17:56:36 -0400

    automated assignment of user/story ids

--- db_tool.py
+++ db_tool.py
@@ -25,13 +25,20 @@ def make_database():
 
 
 # add a user to the users table
-def add_user( user_id, username, password ):
+def add_user( username, password ):
     # open the database
     db = sqlite3.connect("app.db")
     c = db.cursor()
+
+    # set the user_id
+    command = "SELECT count(*) FROM users;"
+    for row in c.execute(command):
+        user_id = row[0]
+
     # do the command
-    command = "INSERT INTO users VALUES( %d, %s, %s )" % (user_id, repr(username), repr(password))
+    command = "INSERT INTO users VALUES( %d, %s, %s );" % (user_id, repr(username), repr(password))
     c.execute(command)
+
     # commit and close the database
     db.commit()
     db.close()
@@ -40,13 +47,20 @@ def add_user( user_id, username, password ):
 
 # add a story to the story table
 # for now, always make completed = 0
-def add_story( story_id, title, body, completed ):
+def add_story( title, body ):
     # open the database
     db = sqlite3.connect("app.db")
     c = db.cursor()
+
+    # set the story_id
+    command = "SELECT count(*) FROM stories;"
+    for row in c.execute(command):
+        story_id = row[0]
+
     # do the command
-    command = "INSERT INTO stories VALUES( %d, %s, %s, %d )" % (story_id,repr(title), repr(body), completed)
+    command = "INSERT INTO stories VALUES( %d, %s, %s, 0 );" % (story_id,repr(title), repr(body) )
     c.execute(command)
+
     # commit and close the database
     db.commit()
     db.close()
@@ -64,14 +78,14 @@ def add_cont( user_id, story_id, addition ):
     # get the timestamp for the contribution
     timestamp = str( datetime.now() )
     # making the actual contribution
-    command = "INSERT INTO contributions VALUES( %d, %d, %s, %s )" % (user_id, story_id, repr(timestamp), repr(addition) )
+    command = "INSERT INTO contributions VALUES( %d, %d, %s, %s );" % (user_id, story_id, repr(timestamp), repr(addition) )
     c.execute(command)
 
     # update the body of the story
-    command = "SELECT body FROM stories WHERE stories.story_id = %d" % (story_id) # get the original text
+    command = "SELECT body FROM stories WHERE stories.story_id = %d;" % (story_id) # get the original text
     for row in c.execute(command):
         body = row[0] + "\n\n" + addition # add the new text
-    command = "UPDATE stories SET body = \"%s\" WHERE stories.story_id = %d" % ( body, story_id )
+    command = "UPDATE stories SET body = \"%s\" WHERE stories.story_id = %d;" % ( body, story_id )
     c.execute(command)
 
     # commit and close the database
@@ -91,8 +105,8 @@ if __name__ == "__main__":
     # 2. Add the command bellow
     # 3. Run the file
 
-    add_user(0, "bob", "pass")
-    add_user(1, "jim", "random")
-    add_user(2, "unknown", "bird")
+    add_user("bob", "pass")
+    add_user("jim", "random")
+    add_user("unknown", "bird")
 
-    add_story(0, "first story", "this is the first story body", 0)
+    add_story("first story", "this is the first story body")
