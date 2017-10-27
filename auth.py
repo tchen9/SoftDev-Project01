9ebbbaf28fbe0bff0fe586c496a22f6917339319
Ray Jones <ajones@stuy.edu>
2017-10-27 11:40:33 -0400

    changed db_method to db_tool

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from flask import session
 import hashlib
-import db_methods
+import db_tool
 
 # TODO: db_method names are placeholders until functions are written
 # TODO: username v ID as parameters
@@ -8,7 +8,7 @@ import db_methods
 # Attempts to log in a user given their username and password
 # Returns 0 for success, 1 for incorrect password, 2 for username does not exist
 def login(username, password):
-    if username in db_methods.get_users():
+    if username in db_tool.get_users():
         if check_password(username, password):
             session['username'] = username
             return 0
@@ -29,18 +29,18 @@ def logout():
 
 # Checks if user's unhashed password is equal to a plaintext string
 def check_password(username, password_to_check):
-    return db_methods.get_user(username)['password'] == hashlib.sha224(password_to_check).hexdigest()
+    return db_tool.get_user(username)['password'] == hashlib.sha224(password_to_check).hexdigest()
 
 # Sets the user's password to a hashed version of the input
 def set_password(username, new_password):
-    return db_methods.set_user_password(hashlib.sha224(new_password).hexdigest())
+    return db_tool.set_user_password(hashlib.sha224(new_password).hexdigest())
 
 # Attempts to create a user given their username, password, and retyped password
 # Returns 0 for success, 1 for unmatching passwords, 2 for username already exists
 def create(username, password1, password2):
-    if not username in db_methods.get_users():
+    if not username in db_tool.get_users():
         if password1 == password2:
-            db_methods.add_user(username, password1)
+            db_tool.add_user(username, password1)
             login(username, password1)
             return 0
         return 1
